047cdcca36b5a099789ef2ed5cd8e8291be02bf0
rsikora96 <roksana.sikora@interia.pl>
2022-10-02 12:38:34 +0200

    deploy portfolio app

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profileImg from '../assets/png/profileImg2.jpg'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    name: "Roksana Sikora",
+    title: "Software Tester",
+    desciption:"My name is Roksana Sikora and I'm looking for opportunity to work as Junior Software Tester.",
+    image: profileImg,
     resumePdf: resume
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,13 @@
 export const socialsData = {
-    github: 'https://github.com/',
+    github: 'https://github.com/rsikora96',
     facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in',
+    linkedIn: 'https://www.linkedin.com/in/roksana-sikora-b293a7210/',
     instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/',
     twitter: 'https://twitter.com/',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
     gitlab: 'https://gitlab.com/',
     youtube: 'https://youtube.com/'
 }
